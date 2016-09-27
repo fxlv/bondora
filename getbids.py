42b4fa9259edfedc67e1f8e92cf8ff5b55f0e541
Kaspars Mickevics <kaspars@fx.lv>
2016-09-27 22:21:45 +0300

    Pretty table

--- getbids.py
+++ getbids.py
@@ -3,11 +3,18 @@ import os
 import sys
 import requests
 import account
+from prettytable import PrettyTable
 
 #
 # Docs:
 # https://api.bondora.com/Intro
 #
+def bidstatus(status_code):
+    known_status_codes = {0:"Pending", 1:"Open", 2:"Successful", 3:"Failed", 4:"Cancelled", 5:"Accepted"}
+    if status_code in known_status_codes:
+        return known_status_codes[status_code] 
+    else:
+        return "Unknown"
 
 url = "https://api.bondora.com/api/v1/bids"
 
@@ -27,7 +34,10 @@ if not response_json['Success']:
     print "We have response, but it's not a success"
     sys.exit(1)
 
+t = PrettyTable()
+t.field_names = ["AuctionId", "ActualBidAmount", "RequestedBidAmount", "StatusCode", "IsRequestBeingProcessed", "BidRequestedDate","BidProcessedDate"]
 payload = response_json['Payload']
+for line in payload:
+    t.add_row([line["AuctionId"],line["ActualBidAmount"],line["RequestedBidAmount"],bidstatus(line["StatusCode"]),line["IsRequestBeingProcessed"],line["BidRequestedDate"],line["BidProcessedDate"]])
 
-
-print response_json
+print t
