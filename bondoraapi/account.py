3c55129c310d8d4b095f8ade5b791df893525eba
Kaspars Mickevics <kaspars@fx.lv>
2016-10-16 12:23:09 +0300

    Added docstring

--- bondoraapi/account.py
+++ bondoraapi/account.py
@@ -11,10 +11,17 @@ import yaml
 
 
 class Account(object):
+    """
+    Bondora account settings.
+    Finds and reads the config file and stores the configuration
+    settings as object attributes.
+    """
 
     def __init__(self):
         self.load()
         self.token = self.config['token']
+        self.config = None
+        self.config_path = None
         # initialize with defaults if needed
         if "accepted_loan_ratings" in self.config:
             self.accepted_loan_ratings = self.config["accepted_loan_ratings"]
@@ -38,6 +45,7 @@ class Account(object):
             self.min_bid = 5
 
     def load(self):
+        "Find and load the configuration file"
         config_file = "config.yaml"
         search_paths = [os.getcwd(), "{}/.bondora".format(os.environ["HOME"])]
 
@@ -46,6 +54,7 @@ class Account(object):
             if os.path.exists(path):
                 with open(path) as config_file:
                     self.config = yaml.load(config_file)
+                    self.config_path = path
                     if "token" not in self.config:
                         msg = "Account Token missing. Check your configuration!"
                         raise KeyError(msg)
