11b90201a99d5a04195d0538e60f22e161c85c1d
Kaspars Mickevics <kaspars@fx.lv>
2016-10-17 21:39:08 +0300

    Save and get values

--- storage.py
+++ storage.py
@@ -1,6 +1,13 @@
 #
 # Storage object for Bondora client
 #
+# Store various operational data, like:
+# - last success
+# - last failure
+# - last successfull bid
+# etc.
+#
+#
 import pickle
 import os
 import datetime
@@ -15,11 +22,23 @@ class Storage(object):
         if os.path.exists(self.path):
             with open(self.path, "rb") as storage_file:
                 self.storage = pickle.load(storage_file)
-                logging.debug("file loaded")
-                logging.debug("Last save was: {}".format(self.storage["last_save"]))
+                logging.debug("Storage file loaded")
+                logging.debug("Last save was: {}".format(
+                    self.storage["last_save"]))
         else:
-            logging.debug("storage file does not exist yet. Returning empty dict")
+            logging.debug(
+                "storage file does not exist yet. Returning empty dict")
             self.storage = {}
+
+    def __getattr__(self, key):
+        logging.debug("Key '{}' requested from Storage".format(key))
+        if key in self.storage:
+            return self.storage[key]
+        else:
+            return None
+    def save(self, key, value):
+        logging.debug("Setting key '{}' = '{}'".format(key, value))
+        self.storage[key] = value
 
     def __del__(self):
         logging.debug("Storage cleanup in progress")
